b3427542980d6aac30a2fd20f2fbb0e06f3f11ce
chbu <chbu@dtu.dk>
2019-09-05 10:03:42 +0200

    Adding webapp

---
+++ src/main/webapp/precache-manifest.543774e7a5422122a5a8a18606639fe0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e975793a9f84c2558c521d28b0e34dab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "916f01876897bd3db4e9",
+    "url": "/static/css/main.ba49ec77.chunk.css"
+  },
+  {
+    "revision": "b4197565ddff3d74f874",
+    "url": "/static/js/2.765fc547.chunk.js"
+  },
+  {
+    "revision": "916f01876897bd3db4e9",
+    "url": "/static/js/main.9d9d20d9.chunk.js"
+  },
+  {
+    "revision": "ebbdfc06dc7c9cd9f127",
+    "url": "/static/js/runtime~main.d8d47c67.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
---
+++ src/main/webapp/precache-manifest.8b67b26617437a4edee517b8a99009e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "190aae2ce66d4b4154191f7f8f212aee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af0d05218df3f4d32b66",
+    "url": "/static/css/main.ba49ec77.chunk.css"
+  },
+  {
+    "revision": "b4197565ddff3d74f874",
+    "url": "/static/js/2.765fc547.chunk.js"
+  },
+  {
+    "revision": "af0d05218df3f4d32b66",
+    "url": "/static/js/main.95543f1b.chunk.js"
+  },
+  {
+    "revision": "ebbdfc06dc7c9cd9f127",
+    "url": "/static/js/runtime~main.d8d47c67.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
