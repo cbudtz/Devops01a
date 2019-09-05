b6d633ba24193989ca37f1c73cf940b32fb8f611
chbu <chbu@dtu.dk>
2019-09-05 10:00:06 +0200

    Custom Profile for HEROKU

--- web/src/App.js
+++ web/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Glædelig DevOps!
+          Glædelig DevOps! E19 - 5/9 - ver2
         </p>
         <a
           className="App-link"
