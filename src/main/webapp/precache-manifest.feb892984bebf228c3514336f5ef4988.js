11cdd45e9d980cc9f0c76cf043fe5996633bce91
chbu <chbu@dtu.dk>
2019-09-16 08:36:30 +0200

    JS commit

---
+++ src/main/webapp/precache-manifest.feb892984bebf228c3514336f5ef4988.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02962feb66a6c2566a533454b9113a94",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a1f5c568b451cf6a108f",
+    "url": "/static/css/main.ba49ec77.chunk.css"
+  },
+  {
+    "revision": "c67f02722116ba6aef95",
+    "url": "/static/js/2.a9495886.chunk.js"
+  },
+  {
+    "revision": "a1f5c568b451cf6a108f",
+    "url": "/static/js/main.84714151.chunk.js"
+  },
+  {
+    "revision": "ebbdfc06dc7c9cd9f127",
+    "url": "/static/js/runtime~main.d8d47c67.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
