97eea9a7ac9240a7e5d840790045042979cec54d
cbudtz <chrbudtz@gmail.com>
2019-09-26 17:48:28 +0200

    Added jUnit engine - now tests are getting run

--- src/test/java/rest/GiraffeServiceTest.java
+++ src/test/java/rest/GiraffeServiceTest.java
@@ -6,14 +6,12 @@ import org.junit.jupiter.api.Test;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class GiraffeServiceTest {
     GiraffeService g = new GiraffeService();
 
     @Test
     void getGiraffes() {
-        List<String> strings = Arrays.asList("Melman", "Eslmer");
+        List<String> strings = Arrays.asList("Melman", "Elmer");
         Assertions.assertEquals(strings, g.getGiraffes());
     }
 }
